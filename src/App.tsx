55db5852936e7115d85e97b26ef973bc0d17dadd
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-02 15:15:12 -0300

    settings global styles config with styled-componets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello world</h1>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
